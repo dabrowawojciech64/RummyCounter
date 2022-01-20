4e57f476bb9bb990629d55fc05d1bdc7f6bc42ea
dabrowawojciech64 <51543327+dabrowawojciech64@users.noreply.github.com>
2022-01-20 19:36:41 +0100

    Add storage functionality

--- Rummy/src/app/common/materials/materials.module.ts
+++ Rummy/src/app/common/materials/materials.module.ts
@@ -5,9 +5,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon'
 
 
-const MaterialComponents = [MatInputModule, MatFormFieldModule,MatCardModule, MatSliderModule,MatButtonModule,MatDialogModule];
+const MaterialComponents = [MatInputModule, MatFormFieldModule,MatCardModule, MatSliderModule,MatButtonModule,MatDialogModule, MatIconModule];
 
 @NgModule({
   imports: [MaterialComponents],
--- Rummy/src/app/dialogs/player-names-dialog/player-names-dialog.component.ts
+++ Rummy/src/app/dialogs/player-names-dialog/player-names-dialog.component.ts
@@ -1,5 +1,7 @@
+import { PlayerData } from 'src/app/common/player-data.PlayerData';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { StorageMap, JSONSchema } from '@ngx-pwa/local-storage';
 
 @Component({
   selector: 'app-player-names-dialog',
@@ -8,13 +10,23 @@ import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
 })
 export class PlayerNamesDialogComponent implements OnInit {
 
+  schema: JSONSchema = {
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        name: { type: 'string' },
+        score: {type: 'number'}
+      }
+    },
+  };
+
+  players?: PlayerData[];
+
+
   playerNamesForm = this.fb.group(
     {
-      names: this.fb.array([
-        this.fb.control('Player 1',[Validators.required]),
-        this.fb.control('Player 2',[Validators.required]),
-      ]
-      )
+      names: this.fb.array([ ])
     }
   );
 
@@ -27,16 +39,64 @@ export class PlayerNamesDialogComponent implements OnInit {
     this.names.push(this.fb.control('',[Validators.required]));
   }
 
-
+  removeName(index: number) {
+    this.names.removeAt(index);
+  }
 
 
   onSubmit() {
+    // you are losing steam so let's review
+    // you need to get names of new players forom this.playerNamesForm.value (it's an array of names)
+    // then assign it to new PlayerData[] where name equals the name and score is 0
+    console.warn(this.playerNamesForm.value.names);
+
+    let names: string[] = this.playerNamesForm.value.names;
+    let newPlayers: PlayerData[] = [];
+    for (const name of names) {
+
+      newPlayers.push({ name: name, score: 0 });
+    }
+    this.storage.set('players', newPlayers,this.schema).subscribe(() => {});
 
   }
+  logPlayers() {
+    console.table(this.players);
 
-  constructor(private fb:FormBuilder) { }
+  }
+  clear() {
+    this.storage.clear().subscribe(() => { });
+  }
+
+  populateForm() {
+    if (this.players == undefined)
+    {
+      console.log("Didn't find no playas");
+      this.names.push(this.fb.control('Player 1', [Validators.required]));
+      this.names.push(this.fb.control('Player 2',[Validators.required]));
+
+    }
+    else {
+      console.log("found something");
+      console.table(this.players);
+
+      for (const player of this.players) {
+        this.names.push(this.fb.control(player.name,[Validators.required]));
+      }
+    }
+  }
+
+  constructor(private fb:FormBuilder, private storage: StorageMap) { }
 
   ngOnInit(): void {
+
+
+
+    this.storage.get('players', this.schema).subscribe((players) => {
+      this.players = <PlayerData[]>players;
+      this.populateForm();
+     });
+
+
   }
 
 }
